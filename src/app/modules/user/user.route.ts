f8e2c33cede7b7b0239b5e0681cf084a90535b6a
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-12-13 22:58:12 +0600

    Admin Functionality Added

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,6 +3,7 @@ import { UserController } from './user.controller';
 import { createStudentValidationSchema } from '../students/student.validation';
 import validateRequest from '../../middlewares/validateRequest';
 import { createFacultyValidationSchema } from '../faculty/faculty.validation';
+import { createAdminValidationSchema } from '../admin/admin.validation';
 
 const router = express.Router();
 
@@ -18,9 +19,9 @@ router.post(
   UserController.createFaculty,
 );
 
-// router.post(
-//   '/create-admin',
-//   validateRequest(createAdminValidationSchema),
-//   UserController.createAdmin,
-// );
+router.post(
+  '/create-admin',
+  validateRequest(createAdminValidationSchema),
+  UserController.createAdmin,
+);
 export const userRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,6 +5,7 @@ import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSeme
 import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route';
 import { FacultyRoutes } from '../modules/faculty/faculty.route';
+import { AdminRoutes } from '../modules/admin/admin.route';
 
 const router = Router();
 
@@ -33,10 +34,10 @@ const moduleRoutes = [
     path: '/faculties',
     route: FacultyRoutes,
   },
-  // {
-  //   path: '/admins',
-  //   route: AcademicDepartmentRoutes,
-  // },
+  {
+    path: '/admins',
+    route: AdminRoutes,
+  },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
